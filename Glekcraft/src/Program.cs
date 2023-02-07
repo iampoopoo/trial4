99cef40920f91380afc12716b28b8f8faa68d3fe
G'lek Tarssza <glek@glektarssza.com>
2023-02-07 04:54:47 -0700

    Set up main.

--- Glekcraft/src/Program.cs
+++ Glekcraft/src/Program.cs
@@ -1,1 +1,17 @@
-Console.WriteLine("Hello world!");
+namespace Glekcraft;
+
+/// <summary>
+/// The class containing the program entry point.
+/// </summary>
+public static class Program {
+    #region Public Static Methods
+
+    /// <summary>
+    /// The program entry point.
+    /// </summary>
+    public static void Main() {
+        Console.WriteLine("Hello world!");
+    }
+
+    #endregion
+}
