7dea471a0ed466385d809d41afce73e752cce989
G'lek Tarssza <glek@glektarssza.com>
2023-03-22 11:05:46 -0600

    Simple main.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file src/main.cpp
+ * @author G'lek Tarssza
+ * @copyright Copyright (c) 2023 G'lek Tarssza
+ * @copyright All rights reserved.
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Hello world!" << std::endl;
+    return EXIT_SUCCESS;
+}
