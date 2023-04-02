43a7b9dfaeae95094f2ebbb61e321deb02df76b0
G'lek Tarssza <glek@glektarssza.com>
2023-04-01 22:14:25 -0600

    [GLFW] Added native error code enum.

---
+++ packages/GLFW/src/ErrorCode.cs
@@ -0,0 +1,11 @@
+namespace Glekcraft.GLFW;
+
+/// <summary>
+/// An enumeration of possible error codes the native library can raise.
+/// </summary>
+public enum ErrorCode {
+    /// <summary>
+    /// The default error code for no errors.
+    /// </summary>
+    NoError = 0
+}
