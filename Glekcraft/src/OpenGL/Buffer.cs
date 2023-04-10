f9e278e1c9147b31bca928fba257af2f0cafa715
G'lek Tarssza <glek@glektarssza.com>
2023-04-10 09:54:56 -0600

    [Main] Removed hack to deal with Silk.NET bug.

--- Glekcraft/src/OpenGL/Buffer.cs
+++ Glekcraft/src/OpenGL/Buffer.cs
@@ -287,7 +287,7 @@ public class Buffer : IDisposable {
         if (!IsBound) {
             throw new InvalidOperationException("Cannot upload data to an unbound OpenGL buffer");
         }
-        Context.BufferData<T0>(Target, (nuint)(data.Length * Marshal.SizeOf<T0>()), data, usage);
+        Context.BufferData<T0>(Target, data, usage);
         var err = (ErrorCode)Context.GetError();
         if (err != ErrorCode.NoError) {
             throw new GLException(err, "Failed to upload data to an OpenGL buffer");
