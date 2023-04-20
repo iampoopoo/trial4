dbaa791ef29d38a1a7e7f6f2c1235cbd9de16d6f
G'lek Tarssza <glek@glektarssza.com>
2023-04-20 16:14:17 -0600

    [Main] Added an OpenGL object type enum.

---
+++ Glekcraft/src/Graphics/Primitives/GLObjectType.cs
@@ -0,0 +1,26 @@
+namespace Glekcraft.Graphics.Primitives;
+
+/// <summary>
+/// An enumeration of types of OpenGL objects.
+/// </summary>
+public enum GLObjectType {
+    /// <summary>
+    /// An OpenGL buffer.
+    /// </summary>
+    Buffer,
+
+    /// <summary>
+    /// An OpenGL shader object.
+    /// </summary>
+    Shader,
+
+    /// <summary>
+    /// An OpenGL shader program object.
+    /// </summary>
+    ShaderProgram,
+
+    /// <summary>
+    /// An OpenGL texture object.
+    /// </summary>
+    Texture
+}
